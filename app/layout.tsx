d3ae2dd103b4b9c35c2af7e5f680e6aff291a7e4
sahilq312 <quraishisahil04@outlook.com>
2024-05-19 16:30:59 +0530

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 import { cn } from "@/lib/utils";
 
 const fontSans = FontSans({
@@ -26,6 +27,7 @@ export default function RootLayout({
           fontSans.variable
         )}
       >{children}
+      <Analytics/>
       </body>
     </html>
   );
